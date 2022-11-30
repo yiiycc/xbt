a23fb26569ba0209ac5209f801fa985e5fa5e23f
yiiycc <41641633+yiiycc@users.noreply.github.com>
2022-11-30 21:51:00 +0900

    Add files via upload

---
+++ misc/tf_misc.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+std::string encode_field(const std::string&, const std::string& local_domain_url);
+std::string encode_text(const std::string&, const std::string& local_domain_url, bool add_span);
+std::string generate_random_string(int l);
+std::string trim_field(const std::string&);
+std::string trim_text(const std::string&);
